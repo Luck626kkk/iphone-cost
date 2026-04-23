7bf8061fae39a1958e9219dc483d0fca7df3746a
luck <aaa26905677a@gmail.com>
2026-04-23 15:41:56 +0800

    fix: correct createRequestHandler API usage for cloudflare workers

--- workers/app.ts
+++ workers/app.ts
@@ -1,10 +1,15 @@
 import { createRequestHandler } from "@react-router/cloudflare";
 import * as build from "../build/server";
 
-const requestHandler = createRequestHandler(build);
+const handleRequest = createRequestHandler({ build });
 
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext) {
-    return requestHandler(request);
+    return handleRequest({
+      request,
+      env,
+      waitUntil: ctx.waitUntil.bind(ctx),
+      passThroughOnException: ctx.passThroughOnException.bind(ctx),
+    });
   },
 } satisfies ExportedHandler<Env>;
